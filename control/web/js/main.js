d8fb59e79b47c627f348c817a5116e3babee7535
Galindo77 <galindoadair77@gmail.com>
2024-05-14 12:37:22 -0600

    Add files via upload

---
+++ control/web/js/main.js
@@ -0,0 +1,33 @@
+//alert("WELCOME");
+let adelante=document.getElementById("adelante");
+let atras=document.getElementById("atras");
+let derecha=document.getElementById("derecha");
+let izquierda=document.getElementById("izquierda"); 
+let detener=document.getElementById("detener");
+let mensaje=document.getElementById("mensaje");
+ function callApi(estatus){
+    // Hacer una petición para un usuario con ID especifico
+axios.get('http://localhost/iot-car-control/back-end/apis/getRegistro.php?valorEstatus='+ estatus)
+.then(function (response) {
+  // manejar respuesta exitosa
+  console.log(response);
+  mensaje.innerHTML="Respuesta: <strong>"+ response.data + "</strong>";
+})
+.catch(function (error) {
+  // manejar error
+  console.log(error);
+})
+.finally(function () {
+  // siempre sera executado
+});
+ }
+adelante.addEventListener("click",function(){
+callApi('f');});
+atras.addEventListener("click",function(){
+callApi('b');});
+derecha.addEventListener("click",function(){
+callApi('r');});  
+izquierda.addEventListener("click",function(){
+callApi('l');});
+detener.addEventListener("click",function(){
+callApi('s');});
