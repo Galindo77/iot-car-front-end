d28f35868f31cce99817da9bb49a57e98dca7496
Galindo77 <galindoadair77@gmail.com>
2024-05-23 11:51:11 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -1,10 +1,10 @@
 //alert("Hola mundo");
-let adelante=document.getElementById("adelante");
-let atras=document.getElementById("atras");
-let izquierda=document.getElementById("izquierda");
-let derecha=document.getElementById("derecha");
-let detener=document.getElementById("detener");
-let mensaje=document.getElementById("mensaje");
+let adelante=document.getElementById("Adelante");
+let atras=document.getElementById("Atras");
+let izquierda=document.getElementById("Izquierda");
+let derecha=document.getElementById("Derecha");
+let detener=document.getElementById("Detener");
+let mensaje=document.getElementById("Mensaje");
 
 
 // Función para llamar a la API
