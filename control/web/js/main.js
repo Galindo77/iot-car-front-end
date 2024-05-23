c112908da59ac1b438bd0097b642ff1c77213fa5
Galindo77 <galindoadair77@gmail.com>
2024-05-23 11:36:04 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -1,33 +1,71 @@
-//alert("WELCOME");
-let adelante=document.getElementById("Adelante");
-let atras=document.getElementById("Atras");
-let derecha=document.getElementById("Derecha");
-let izquierda=document.getElementById("Izquierda"); 
-let detener=document.getElementById("Detener");
-let mensaje=document.getElementById("Mensaje");
- function callApi(estatus){
+//alert("Hola mundo");
+let adelante=document.getElementById("adelante");
+let atras=document.getElementById("atras");
+let izquierda=document.getElementById("izquierda");
+let derecha=document.getElementById("derecha");
+let detener=document.getElementById("detener");
+let mensaje=document.getElementById("mensaje");
+
+
+// Función para llamar a la API
+
+function CallApi(estatus)
+{
     // Hacer una petición para un usuario con ID especifico
-axios.get('http://3.142.153.159/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+ estatus)
-.then(function (response) {
-  // manejar respuesta exitosa
-  console.log(response);
-  mensaje.innerHTML="Respuesta: <strong>"+ response.data + "</strong>";
-})
-.catch(function (error) {
-  // manejar error
-  console.log(error);
-})
-.finally(function () {
-  // siempre sera executado
-});
- }
-adelante.addEventListener("click",function(){
-callApi('f');});
-atras.addEventListener("click",function(){
-callApi('b');});
-derecha.addEventListener("click",function(){
-callApi('r');});  
-izquierda.addEventListener("click",function(){
-callApi('l');});
-detener.addEventListener("click",function(){
-callApi('s');});
+    axios.get('http://3.142.153.159/iot-car-control/back-end/apis/setRegistro.php?valorEstatus='+estatus)
+    .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+    mensaje.innerHTML="Respuesta: " + response.data;
+    })
+    .catch(function (error) {
+    // manejar error
+    console.log(error);
+    })
+    .finally(function () {
+    // siempre sera executado
+    });
+}
+
+
+
+
+if (adelante) {
+  adelante.addEventListener("click", function () {
+      CallApi('F');
+  });
+} else {
+  console.error("El elemento con el ID 'adelante' no existe.");
+}
+
+if (atras) {
+  atras.addEventListener("click", function () {
+      CallApi('B');
+  });
+} else {
+  console.error("El elemento con el ID 'adelante' no existe.");
+}
+
+if (izquierda) {
+  izquierda.addEventListener("click", function () {
+      CallApi('L');
+  });
+} else {
+  console.error("El elemento con el ID 'adelante' no existe.");
+}
+
+if (derecha) {
+  derecha.addEventListener("click", function () {
+      CallApi('R');
+  });
+} else {
+  console.error("El elemento con el ID 'adelante' no existe.");
+}
+
+if (detener) {
+  detener.addEventListener("click", function () {
+      CallApi('S');
+  });
+} else {
+  console.error("El elemento con el ID 'adelante' no existe.");
+}
